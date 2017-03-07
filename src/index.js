b55dca8d1279402e7b0707930a0e220e7c11693a
Ian Andersen <ianderse@mac.com>
2017-03-07 09:04:56 -0700

    basic material-ui setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import AppWithNavigation from './navigation';
 import store from './store';
+import { ThemeProvider } from 'react-native-material-ui';
 
 class MyApp extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppWithNavigation/>
+        <ThemeProvider>
+          <AppWithNavigation/>
+        </ThemeProvider>
       </Provider>
     );
   }
